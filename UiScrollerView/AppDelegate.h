45bdaf0a8982f7e5fe0fdb42a58f49b8f5979c88
weiwenwang <641257395@qq.com>
2014-11-09 07:23:01 +0800

    Initial Commit

---
+++ UiScrollerView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UiScrollerView
+//
+//  Created by wangweiwen on 14-11-9.
+//  Copyright (c) 2014年 wangweiwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
